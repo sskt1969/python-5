881fc22b4fabccc641b24d316c2e1ec1c53b4b3a
sskt1969 <41769896+sskt1969@users.noreply.github.com>
2018-08-03 02:25:59 -0700

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,11 @@
+num=(input("Enter num:"))
+temp=num
+rev=0
+while(num>0):
+    dig=num%10
+    rev=rev*10+dig
+    num=num//10
+if(temp==rev):
+    print("The number is a palindrome!")
+else:
+    print("The number isn't a palindrome!")
